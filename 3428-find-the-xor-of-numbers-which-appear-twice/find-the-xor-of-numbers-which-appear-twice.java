48800c669ca954d2186f62d0ea46209142b95f35
Amit Dubey <amitdubey04102004@gmail.com>
2026-07-26 16:01:17 +0530

    Time: 3 ms (36.44%) | Memory: 45.4 MB (10.83%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -1,20 +1,23 @@
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
+// hamko isme hashmap use kr k value ko find krna hai ye pura xor nahi solve kr payega
 
-        for(int num: nums){
-            map.put(num,map.getOrDefault(num, 0)+ 1);
+        for (int num : nums) {
+            // ek ek kr k array ko travse krega 
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-            int ans = 0;
-            for(Map.Entry<Integer,  Integer> entry : map.entrySet()){
-                int number = entry.getKey();
-                int freq = entry.getValue();
-
-            if (freq == 2){
+        // sare values ko dekhna padega  vo kaise hai en
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int number = entry.getKey();
+            int freq = entry.getValue();
+// nikale huye value ko dekhega vo kaise hai or unka oxr find kr k rakhega
+            if (freq == 2) {
                 ans ^= number;
             }
-            
+
         }
-            return ans;
-}
+        return ans;
+    }
 }
