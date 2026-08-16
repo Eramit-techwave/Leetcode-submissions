bbdf82bed106aaf6d432c55d2dfdd4974d957fb5
Amit Dubey <amitdubey04102004@gmail.com>
2026-08-16 13:09:00 +0530

    Time: 3 ms (94.03%) | Memory: 44 MB (97.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // checking the length of the string if the length is not smae eturn false
         if(s.length() != t.length()){
             return false;
         }
-        int[] fre = new int[26];
+        int[] fre = new int[26]; // converting all of them into a charcteer
+        // checking the length with s string and incresing the count of the string length
         for(int i  =0; i< s.length(); i++){
             fre[s.charAt(i) - 'a'] ++;
         }
-
+// second loop for the second t string length and decresing the length;
         for(int i =0; i <t.length();i++){
             fre[t.charAt(i) - 'a']--;
         }
